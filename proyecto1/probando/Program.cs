e50389afe89d9c73f4bb7a2734f60be88e667681
Erick Téquiz <68562988+Erick-Tx@users.noreply.github.com>
2025-03-04 22:55:46 -0500

    secret

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Erick!");
+
+{
+    "ConnectionStrings": {
+        "DefaultConnection": "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;"
+    }
+}
